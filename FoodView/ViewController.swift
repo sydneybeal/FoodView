d0e634fc0e42c730ea35a69bee0a339b8c181751
Sydney Beal <sydneybeal@miami.edu>
2018-10-10 15:39:44 -0400

    Initial Commit

---
+++ FoodView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodView
+//
+//  Created by Sydney Beal on 10/10/18.
+//  Copyright © 2018 Sydney Beal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
